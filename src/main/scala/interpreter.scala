3667131e2dc9ee58ee4f27c1a7980a4a5a08b95a
Arnold deVos <adv@bgsig.com>
2021-01-26 15:08:56 +1100

    remove uwanted types

---
+++ src/main/scala/interpreter.scala
@@ -0,0 +1,16 @@
+package machines
+
+import scala.collection.mutable
+
+class MultiMap[K[_], V[_]](underlying: mutable.Map[Any, mutable.Buffer[Any]]):
+  def get[A](k: K[A]): Iterable[V[A]] =
+    underlying.get(k).fold(Iterable.empty[V[A]])(_.asInstanceOf[Iterable[V[A]]])
+
+class MapBuilder[K[_], V[_]]:
+  val underlying = mutable.Map[Any, mutable.Buffer[Any]]()
+
+  def add[A](k: K[A], v: V[A]): Unit =
+    if underlying.contains(k) then underlying(k).append(v)
+    else underlying(k) = mutable.Buffer(v)
+
+  def result = MultiMap[K, V](underlying)
