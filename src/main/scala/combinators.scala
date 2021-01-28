9c461c22a9bef656c686bc5ab3e69406f103beed
Arnold deVos <adv@bgsig.com>
2021-01-28 22:21:50 +1100

    tweak combinators

--- src/main/scala/combinators.scala
+++ src/main/scala/combinators.scala
@@ -16,10 +16,13 @@ enum Process[-A, +B, +C]:
   case Pipe[A, B, X](stage1: Transducer[A, X], stage2: Transducer[X, B]) extends Transducer[A, B]
   case Broadcast[A](backlog: Int, receivers: Consumer[A]*) extends Consumer[A]
   case Balance[A](receivers: Consumer[A]*) extends Consumer[A]
-  case Publish[A, A1 >: A](tag: Tag[A1]) extends Consumer[A]
-  case Subscribe[B, B1 <: B](tag: Tag[B1], backlog: Int = 3) extends Producer[B]
+  case Concentrate[A](senders: Producer[A]*) extends Producer[A]
+  case Ref[A, A1 >: A](tag: Tag[A1]) extends Consumer[A]
+  case Deref[B, B1 <: B](tag: Tag[B1]) extends Producer[B]
   case Output[A, A1 >: A](tag: Tag[A1]) extends Consumer[A]
   case Input[B, B1 <: B](tag: Tag[B1]) extends Producer[B]
   case System(processes: Process[Nothing, Nothing, Any]*) extends Process[Nothing, Nothing, Any]
+
+  def :->[X >: B, Y](that: Transducer[X, Y]) = Pipe(this, that)
 
 import Process._
