591a46d2799206cd4ed7934beb624d9bfa765a51
Arnold deVos <adv@bgsig.com>
2021-02-06 10:07:07 +1100

    formatting

--- src/main/scala/actors.scala
+++ src/main/scala/actors.scala
@@ -11,18 +11,25 @@ abstract class Actor[A, B, C]:
 
   final def process: Process[A, B, C] = 
     Process.Run(machine)
+
   final def machine: Machine[A, B, C] =
     Defer(this, a => a.start)
+
   final def reactOr(alt: => Cont)(cont: A => Cont): Cont = 
     React(cont, apply1[A, Cont], Defer(() => alt, apply0[Cont]))
+
   final def send(b: B)( cont: => Cont): Cont = 
     Emit(b, Defer(() => cont, apply0[Cont]))
+
   final def stop(c: C): Cont =
     Stop(c)
+
   final def error(t: Throwable): Cont =
     Error(t) 
+
   final def branch(l: => Cont)(r: => Cont): Cont =
     Branch(Defer(() => l, apply0[Cont]), Defer(() => r, apply0[Cont]))
+    
   final def react(cont: A => Cont)(using result: Unit <:< C): Cont = 
     React(cont, apply1[A, Cont], stop(result(())))  
   
