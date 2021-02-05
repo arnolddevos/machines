c89cbbdb3dbda85649fe7ae192d3ce253d68f803
Arnold deVos <adv@bgsig.com>
2021-02-06 10:08:01 +1100

    rename net as continue

--- src/main/scala/machines.scala
+++ src/main/scala/machines.scala
@@ -3,7 +3,7 @@ package machines
 enum Machine[-A, +B, +C]:
   case React[A, B, C, S]( state: S, accept: (S, A) => Machine[A, B, C], close: Machine[A, B, C]) extends Machine[A, B, C]
   case Defer[A, B, C, S]( state: S, advance: S => Machine[A, B, C]) extends Machine[A, B, C]
-  case Emit( b: B, next: Machine[A, B, C])
+  case Emit( b: B, continue: Machine[A, B, C])
   case Branch( react: Machine[A, B, C], emit: Machine[A, B, C])
   case Stop(c: C)
   case Error(e: Throwable)
